40f577c91c8b529ca297bec0afa962ae58b77cd6
Gerard Slee <ddutchie@gmail.com>
2019-08-30 16:47:52 +0200

    Color Fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@
 
 import sphinx_rtd_theme
 
+
 # -- Project information -----------------------------------------------------
 
 project = 'LUX Walker'
@@ -50,10 +51,32 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 
+
+html_logo = 'extra/images/Logosmall.png'
+
+html_theme_options = {
+
+    'canonical_url': '',
+
+    'logo_only': True,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'style_nav_header_background': 'white',
+
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-
+html_css_files = [
+    'css/custom.css',
+]
 master_doc = 'index'
